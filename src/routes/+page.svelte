feedb64ecf76235688c5c23390936141862acd65
Maxime Moreillon <m.moreillon@gmail.com>
2022-12-16 08:56:57 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<TopAppBar bind:this={topAppBar} variant="standard">
+  <Row>
+    <Section>
+      <Title>MESN CRUD</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+  Hello
+</AutoAdjust>
+
+<script lang="ts">
+import TopAppBar, {
+Row,
+Section,
+Title,
+AutoAdjust,
+} from '@smui/top-app-bar';
+import IconButton from '@smui/icon-button';
+
+let topAppBar: TopAppBar;
+</script>
+
